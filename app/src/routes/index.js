f66c70bc81318c7d76406659966af52981ad9b90
Chandler Davidson <chandler.davidson@olo.com>
2022-06-24 09:10:43 -0500

    Filter requests to local

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -4,9 +4,21 @@ import { updateSchedule, deleteSchedule } from "./schedules.js";
 
 export function registerRoutes(app) {
   app.get("/lyrics", (req, resp) => fetchSection(req, resp));
-  app.post("/artist", (req, resp) => insertArtist(req, resp));
+  app.post("/artist", (req, resp) => onlyAllowLocalConnections(req, resp, insertArtist));
 
-  app.post('/schedules', (req, resp) => updateSchedule(req, resp));
-  app.put('/schedules', (req, resp) => updateSchedule(req, resp));
-  app.delete('/schedules/:channelId', (req, resp) => deleteSchedule(req, resp));
+  app.post('/schedules', (req, resp) => onlyAllowLocalConnections(req, resp, updateSchedule));
+  app.put('/schedules', (req, resp) => onlyAllowLocalConnections(req, resp, updateSchedule));
+  app.delete('/schedules/:channelId', (req, resp) => onlyAllowLocalConnections(req, resp, deleteSchedule));
+}
+
+function onlyAllowLocalConnections(req, resp, func) {
+  const host = req.headers.host.split(':')[0];
+  
+  if (host !== 'localhost') {
+    resp.send('Whatcha doin? 👀');
+    
+    return
+  }
+
+  func(req, resp);
 }
